677ada60ffdfb5927eb90d59e1c11cb6691d9099
parasbhandage <112455254+parasbhandage@users.noreply.github.com>
2022-08-30 08:45:49 +0530

    Add files via upload

---
+++ smiley.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+//arc(x,y,r,sAngle,eAngle,counterclockwise);
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+ctx.moveTo(110, 75);
+ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+ctx.moveTo(65, 65);
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+ctx.moveTo(95, 65);
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+ctx.stroke();
